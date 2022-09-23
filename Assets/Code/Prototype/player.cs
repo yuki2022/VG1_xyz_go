2abb8f7a3a6bd1b05f6831bb0beaec321153f733
yuki2022 <97975924+yuki2022@users.noreply.github.com>
2022-09-23 09:53:20 -0500

    Ruowen Xu W4

--- Assets/Code/Prototype/player.cs
+++ Assets/Code/Prototype/player.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    
+    public float jumpSpeed = 0.4f;
+
+    //Outlets
     // Start is called before the first frame update
     void Start()
     {
-        
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //move up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0.02f, 0);
+        }
+        //move down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -0.02f, 0);
+        }
+
+        //move left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.02f, 0, 0);
+        }
+        //move Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.02f, 0, 0);
+        }
+
+
     }
 }
